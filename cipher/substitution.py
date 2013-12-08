707cd1bf0e8267f0beaeee7cc460c52b8b507aa5
DanielLGray <dgray60@gmail.com>
2013-12-08 16:34:37 -0500

    Initial work on subsitution and frequency anaylsis modules

---
+++ cipher/substitution.py
@@ -0,0 +1,75 @@
+import sys
+import random
+
+''' Takes text argument and encrypts/decrypts according to a random key. ''' 
+
+ALPHABET = (
+			'A',
+			'B',
+			'C',
+			'D',
+			'E',
+			'F',
+			'G',
+			'H',
+			'I',
+			'J',
+			'K',
+			'L',
+			'M',
+			'N',
+			'O',
+			'P',
+			'Q',
+			'R',
+			'S',
+			'T',
+			'U',
+			'V',
+			'W',
+			'X',
+			'Y',
+			'Z',
+)
+
+
+
+
+class plaintext(object):
+	
+	def __init__(self):
+		self.name = sys.argv[1]
+		self.text = text 
+	
+	def read_text():
+		
+		with open(sys.argv[1], 'r') as f:
+			self.text = f.read()		
+	
+	
+
+
+
+
+class key(object):
+	
+	def __init__(self):
+		self.dictionary = sys.argv[2]
+		
+	def generate():
+		pass
+
+class cipher(object):
+	
+	def __init(self, text, key):
+		pass
+
+	def encipher():
+		
+		
+		with open(key.name, 'r') as f:
+			r.write(key.dictionary)
+
+	def decipher():
+		pass
+
