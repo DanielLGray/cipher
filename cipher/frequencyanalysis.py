8fcb1c66db41f4f795d266ac9ad6f9a57818fd88
DanielLGray <dgray60@gmail.com>
2013-12-08 17:28:24 -0500

    Plaintext input finished in substitution

---
+++ cipher/frequencyanalysis.py
@@ -0,0 +1,25 @@
+import re
+import sys
+
+
+class ciphertext(object):
+	
+	def __init__(self):
+		self.text = text
+
+		text = sys.argv[1]
+
+class frequency(object):
+	pass
+
+class letter(frequency):
+	pass
+
+class word(frequency):
+	pass
+
+class repeats(frequency):
+	pass
+
+class transpositions(frequency):
+	pass 
