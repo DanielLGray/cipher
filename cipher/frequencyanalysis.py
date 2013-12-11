dec35c7da35219d772e0824bdda86cc367de635b
DanielLGray <dgray60@gmail.com>
2013-12-10 21:14:10 -0500

    Subsitution finished, letter combination repeats started

--- cipher/frequencyanalysis.py
+++ cipher/frequencyanalysis.py
@@ -1,25 +1,67 @@
-import re
-import sys
 
 
 class ciphertext(object):
 	
-	def __init__(self):
+	def __init__(self, text):
 		self.text = text
 
-		text = sys.argv[1]
+		# text = sys.argv[1]
+
+class repeats(object):
+	pass
 
 class frequency(object):
-	pass
 
-class letter(frequency):
-	pass
+	def __init__(self, text):
+		self.text = text
+		self.ALPHABET = [
+			'A',
+			'B',
+			'C',
+			'D',
+			'E',
+			'F',
+			'G',
+			'H',
+			'I',
+			'J',
+			'K',
+			'L',
+			'M',
+			'N',
+			'O',
+			'P',
+			'Q',
+			'R',
+			'S',
+			'T',
+			'U',
+			'V',
+			'W',
+			'X',
+			'Y',
+			'Z',
+		]
+		# Alphabetical list of (letter, frequency) tuples in text.
+		self.frequency = [(LETTER, text.count(LETTER)) for LETTER in self.ALPHABET] 
+		# Tuples of (letter, frequency) sorted by most common to least common letter.
+		self.relativefrequency = sorted(self.frequency, key=lambda x: x[1], reverse=True)
 
-class word(frequency):
-	pass
 
-class repeats(frequency):
-	pass
+ 		nonzerofrequency = [x for x in self.relativefrequency if x[1] != 0]
+
 
 class transpositions(frequency):
 	pass 
+
+
+if __name__ == "__main__":
+
+	text = 'ASDFAWEFADGSRTGREARAGRDGSFGSFDXBCVBDFGFHGJFGHJYUITYUIRYTT'
+
+	a_count = frequency(text)
+
+	# print a_count.frequency, "\n", a_count.relativefrequency
+
+	print a_count.relativefrequency	
+
--- cipher/substitution.py
+++ cipher/substitution.py
@@ -21,16 +21,7 @@ class key(object):
 
 	def __init__(self, dictFilename=None):
 		self.dictFilename = dictFilename
-		
-	def read_key(self, dictFilename):
-		if dictFilename:
-			with open(dictFilename, 'r') as f:
-				self.dictionary = f.read()
-		return self.dictionary
-
-	def generate_key(self):
-
-		ALPHABET = [
+		self.ALPHABET = [
 			'A',
 			'B',
 			'C',
@@ -58,14 +49,22 @@ class key(object):
 			'Y',
 			'Z',
 		]
-		# make a copy of the shuffled list
-		cipherLETTERS = list(ALPHABET)
-		# use random.shuffle
-		shuffle(cipherLETTERS)
-		self.dictionary = dict(zip(ALPHABET, cipherLETTERS))
+
+	def read_key(self, dictFilename):
+		if dictFilename:
+			with open(dictFilename, 'r') as f:
+				self.dictionary = f.read()
 		return self.dictionary
 
+	def generate_key(self):
 
+		
+		# make a copy of the shuffled list
+		cipherLETTERS = list(self.ALPHABET)
+		# use random.shuffle
+		shuffle(cipherLETTERS)
+		self.dictionary = dict(zip(self.ALPHABET, cipherLETTERS))
+		return self.dictionary
 
 class cipher(object):
 	
@@ -73,8 +72,9 @@ class cipher(object):
 		self.plaintext = plaintext
 		self.key = key
 		self.cipherTEXT = ''
+		self.problemLetters = []
 
-	def encipher(self, plaintext, key):
+	def ciphertext(self, plaintext, key):
 		plainTEXT = plaintext.text.upper()		
 		# write as generator next
 		for LETTER in plainTEXT:
@@ -83,8 +83,22 @@ class cipher(object):
 			# figure out what type that these errors are
 			except Exception, e:
 				cipherLETTER = LETTER
+				self.problemLetters.append(LETTER)
 			self.cipherTEXT += cipherLETTER
-		return self.cipherTEXT
+		return self.cipherTEXT, self.problemLetters
+
+	def encipher(self, plaintext, key):
+		self.ciphertext(self, plaintext, key)
+# write with option to remove whitespace
+
+	# with open('cipherTEXT of %s' %basename(plaintext.filname), 'a') as f:
+	# 	f.write(cipherTEXT)
+
+	# with open('generated_key' % plainTEXT.filename, 'a') as f:
+	# 		# Can only print strings, so change dictionary to string.
+	# 	f.write(str(self.dictionary))
+		
+
 		
 	def decipher():
 		pass
@@ -105,15 +119,6 @@ if __name__ == '__main__':
 	someCipher = cipher()
 	a_key = someKey.generate_key()
 	print a_key
-	print someCipher.encipher(someText, someKey)
+	print someCipher.ciphertext(someText, someKey)
 
-	# write with option to remove whitespace
-
-	# with open('cipherTEXT of %s' %basename(plaintext.filname), 'a') as f:
-	# 	f.write(cipherTEXT)
-
-	# with open('generated_key' % plainTEXT.filename, 'a') as f:
-	# 		# Can only print strings, so change dictionary to string.
-	# 	f.write(str(self.dictionary))
-		
-		
+			
