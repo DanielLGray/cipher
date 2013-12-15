6b403c7c01b34f5cbea1869e39c0d1953c6bc387
DanielLGray <dgray60@gmail.com>
2013-12-15 01:16:48 -0500

    Python3 Upgrade

--- cipher/frequencyanalysis.py
+++ cipher/frequencyanalysis.py
@@ -1,4 +1,5 @@
 import re
+from itertools import groupby
 
 class ciphertext(object):
 	
@@ -10,7 +11,7 @@ class ciphertext(object):
 class repeats(object):
 	pass
 
-class frequency(object):
+class analysis(object):
 
 	def __init__(self, text):
 		self.text = text
@@ -43,35 +44,37 @@ class frequency(object):
 			'Z',
 		]
 		
-	def frequency(self, text):
+	def frequency_count(self):
 		# Alphabetical list of (letter, frequency) tuples in text.
 		self.frequency = [(LETTER, text.count(LETTER)) for LETTER in self.ALPHABET] 
+		# Using groupby, dictionary of letter: length of the list of each letter in the sorted text.
+		#  self.frequency = {k:len(list(v)) for k,v in groupby(sorted(text))}
+
 		return self.frequency
 
 	def relative_frequency(self):
-		@frequency()
 		# Tuples of (letter, frequency) sorted by most common to least common letter.
 		self.relativefrequency = sorted(self.frequency, key=lambda x: x[1], reverse=True)
-		return self.relativefrequency
-
+		print(self.relativefrequency)
+		
 	def repeats(self):
  		nonzerofrequency = [x for x in self.relativefrequency if x[1] != 0]
-
+ 		return True
  		
 
-class transpositions(frequency):
+class transpositions(object):
 	pass 
 
 
 if __name__ == "__main__":
 
-	text = 'ASDFAWEFADGSRTGREARAGRDGSFGSFDXBCVBDFGFHGJFGHJYUITYUIRYTT'
+	text = 'AAAAAAAASDFAWEFADGSRTGREARAGRDGSFGSFDXBCVBDFGFHGJFGHJYUITYUIRYTT'
 
-	a_count = frequency(text)
+	a_count = analysis(text)
 
-	# print a_count.frequency, "\n", a_count.relativefrequency
+	print(a_count.frequency_count())
 
-	print a_count.relativefrequency	
+	print(a_count.relative_frequency())
 
 
 
