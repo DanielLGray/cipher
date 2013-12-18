21232f46e1581e929dba317d881fae232c9ce8cb
DanielLGray <dgray60@gmail.com>
2013-12-17 20:36:18 -0500

    Using Count, working on Snippets

--- cipher/frequencyanalysis.py
+++ cipher/frequencyanalysis.py
@@ -1,66 +1,30 @@
 import re
 from itertools import groupby
+from collections import Counter
 
 class ciphertext(object):
 	
 	def __init__(self, text):
 		self.text = text
-
 		# text = sys.argv[1]
-
-class repeats(object):
-	pass
 
 class analysis(object):
 
 	def __init__(self, text):
 		self.text = text
-		self.ALPHABET = [
-			'A',
-			'B',
-			'C',
-			'D',
-			'E',
-			'F',
-			'G',
-			'H',
-			'I',
-			'J',
-			'K',
-			'L',
-			'M',
-			'N',
-			'O',
-			'P',
-			'Q',
-			'R',
-			'S',
-			'T',
-			'U',
-			'V',
-			'W',
-			'X',
-			'Y',
-			'Z',
-		]
 		
-	def frequency_count(self):
+	def frequency(self):
 		# Alphabetical list of (letter, frequency) tuples in text.
-		self.frequency = [(LETTER, text.count(LETTER)) for LETTER in self.ALPHABET] 
-		# Using groupby, dictionary of letter: length of the list of each letter in the sorted text.
-		#  self.frequency = {k:len(list(v)) for k,v in groupby(sorted(text))}
-
-		return self.frequency
-
-	def relative_frequency(self):
-		# Tuples of (letter, frequency) sorted by most common to least common letter.
-		self.relativefrequency = sorted(self.frequency, key=lambda x: x[1], reverse=True)
-		print(self.relativefrequency)
+		# use super to inherit from class Counter()
+		a_count = Counter(text)
+		self.relative_frequency = a_count.most_common()
+		self.frequency = sorted(self.relative_frequency)
+		return self.frequency, self.relative_frequency
 		
-	def repeats(self):
- 		nonzerofrequency = [x for x in self.relativefrequency if x[1] != 0]
- 		return True
- 		
+	def snippets(self):
+		# Calling set on the list returned by findall removes all the repeated letters.
+		two_letters = [(pair[0], set(re.findall(r'(\w)' + pair[0], text))) for pair in self.relative_frequency]
+		return two_letters
 
 class transpositions(object):
 	pass 
@@ -72,9 +36,9 @@ if __name__ == "__main__":
 
 	a_count = analysis(text)
 
-	print(a_count.frequency_count())
 
-	print(a_count.relative_frequency())
+	print(a_count.frequency())
 
+	print(a_count.snippets())
 
 
