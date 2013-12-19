1771b4a99f26faef5f338ecd5dc15044f9edeb55
DanielLGray <dgray60@gmail.com>
2013-12-19 11:33:41 -0500

    iterator finished

--- cipher/frequencyanalysis.py
+++ cipher/frequencyanalysis.py
@@ -22,37 +22,35 @@ class analysis(object):
 		# if the letter is not present in the text, it is not listed in the frequency calculations (even as 0)
 		
 		# outputs largest to smallest frequency {'e': 25, 'a': 12 ...}
-		self.relative_frequency = dict(a_count.most_common())
+		letters_dict = dict(a_count.most_common())
 
 		# outputs letter frequencies alphabetically (a-z) like {'a': 12, 'b': 5... }
-		self.frequency = sorted(self.relative_frequency)
+		self.frequency = sorted(letters_dict)
+	
+		snippets_list = []
 
+		# dangerous! rewrite as generator
+		while True:
 		# eventually use dotall to read over \n
 
 		# in a text of "aaaaabbbbaaaaa"
 		# for the keys in the dictionary of most common letters, use findall to list 
 		# each letter and every letter before it, making a list of lists. [['aa', 'ba'], ['ab', 'bb'], ...]
 		# then, reduce the list of lists into a single list. ['aa', 'bb', 'ab', 'bb', ...]
-		two_letters = reduce(lambda x, y: x + y, ((re.findall(r'(\w{0})'.format(k), text)) for k,v in self.relative_frequency.items()))	
+			snippets = reduce(lambda x, y: x + y, ((re.findall(r'(\w{0})'.format(k), text)) for k,v in letters_dict.items()))
 		
 		# for the long list of all two letter pairs, return a Counter object dictionary
 		# in the order of largest to smallest. {'aa': 1, 'bb': 2, ...}
-		two_letters_dictionary = Counter(two_letters)
+			snippets_frequency = Counter(snippets)
+
 		
 		# filter the dictionary to return pairs that are unique, i.e. greater than one.
 		# like {'bb': 2, ...}
-		letters_dict = {k:v for k, v in two_letters_dictionary.items() if v > 1}
-
-
-
-		snippets_list = []
-		# dangerous! rewrite as generator
-		while True:
-			snippets = reduce(lambda x, y: x + y, ((re.findall(r'(\w{0})'.format(k), text)) for k,v in letters_dict.items()))
-			snippets_frequency = Counter(snippets)
 			nonunique_snippets = {k:v for k, v in snippets_frequency.items() if v > 1}
+			
 			if not nonunique_snippets:
 				break 
+			
 			snippets_list.append(nonunique_snippets)
 			letters_dict = nonunique_snippets
 			
