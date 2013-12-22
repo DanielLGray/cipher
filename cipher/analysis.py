6b17f686cd9297c47e742f1184e2cb2e2e7e8183
DanielLGray <dgray60@gmail.com>
2013-12-22 16:30:11 -0500

    Solved boxcar repeats finally111

--- cipher/analysis.py
+++ cipher/analysis.py
@@ -14,80 +14,93 @@ Frequency class: specify filename if run as a stand-alone, or pass text to class
 
 class Frequency(object):
 
-	def __init__(self, text, minimum_boxcar_length=3):
+	def __init__(self, text, length=3):
 		self.text = text
-		self.minimum_boxcar_length = minimum_boxcar_length
+		self.length = length
 		
-	def boxcar(self, minimum_boxcar_length=3):
+	def boxcar(self, length=3):
+		DEBUG = True
 		ignored_text = ignore_characters.automatic + ignore_characters.user_added
-
-		plaintext = list(text)
-		complete_text = ''.join(plaintext)
-				
-		
+			
 		self.combinations = Counter()
 
-		for n in range(0, len(plaintext)):		
+		end_of_text = False
+		if DEBUG:
+			print "length of text:", len(text)
+			print "length of boxcar", length
 
-			try:
+		last_boxcar = len(text) - length + 1
+		if DEBUG:
+			print "last_boxcar =", last_boxcar
 
-				# if the character isn't one that should be considered, this will
-				# skip this cycle of the iteration.
-				for x in range(0, minimum_boxcar_length):
-					if plaintext[n+x] in ignored_text:
-						continue			
-
-				# makes the boxcar a minimum of two characters
-				# if ValueError (because the second character is beyond the end of list)
-				# will skip this cycle of the iteration.
-
-				boxcar_start_fragments = [plaintext[n]]
-				for x in range(1, minimum_boxcar_length):
-					boxcar_start_fragments.append(plaintext[n+x])
-
-				boxcar = [reduce(lambda x, y: x + y, boxcar_start_fragments)]
-				assert len(boxcar) == 1
-				print boxcar
-				if boxcar[0] in complete_text[:n] or boxcar[0] in complete_text[(n):]:
-					self.combinations.update(boxcar)
-					print "added!"
-				else:
-					print "not added!"
-					continue
-
-
-
-				# Finds plaintext[n+2] and adds to boxcar, until boxcar is equal 
-				# in length to half of the list, since a repeat can only be as long as 
-				# one half of the list, 
-				for i in range(3, len(plaintext)-1):
-					not_in_text = False
-					try:
-
-			 			if plaintext[n+i] in ignored_text:
-							not_in_text = True 
-							break
-
-			 			boxcar = [reduce(lambda x, y: x + y, [boxcar[-1], plaintext[n+i]])]
-						
-
-			 			print boxcar[0], "inbefore", complete_text[:(n-1)], "inafter", complete_text[(n+1):]
-						if boxcar[0] in complete_text[:(n)] or boxcar[0] in complete_text[(n+1):]:
-							self.combinations.update(boxcar)
-							print "added ", boxcar
-						else:
-							not_in_text = True
-							break			
-			 		
-			 		except IndexError:
-			 			break
-					if not_in_text:
-						break			
-
-			except IndexError:
+		for start_letter in range(0, last_boxcar):		
+			if DEBUG:
+				print "\n-----------\ntext[",start_letter, "] range", range(0, last_boxcar), "which is okay because last index in text =", len(text)-1, "and a boxcar of", length
+			if text[start_letter] in ignored_text:
 				continue
 
-			
+			if DEBUG:
+				print "-----------\nstart letter = ", text[start_letter]
+			try:
+				if DEBUG:
+					print "expansion range is", range(0, len(text)-length-start_letter+1)
+				for expansion in range(0, (len(text)-length-start_letter)+1):
+					
+					end_of_expansion = False
+					
+					end_letter = start_letter + length + expansion
+					if DEBUG:
+						print "end_letter = start_letter + length + expansion", end_letter, '=', start_letter, '+', length, '+', expansion
+
+					if end_letter > len(text):
+						if DEBUG:
+							print "end_letter is", end_letter, "and is greater than or equal to len(text)", len(text), "BREAK"
+						end_of_expansion = True
+						break
+					
+					text_slice = text[start_letter:end_letter]
+					
+					if DEBUG:
+						print "indices text[", start_letter, end_letter, "] and text slice is", text_slice
+					
+					if end_letter > len(text):
+						if DEBUG:
+							print "end of expansion... end_letter > len(text) BREAK"
+						end_of_expansion = True
+						break
+					if len(text_slice) == len(text):
+						if DEBUG:
+							print "Not enough space for repeats of a slice of length", len(text_slice), "in", len(text), "BREAK"
+						end_of_expansion = True
+						break
+					try:
+						left_slice = text[:(start_letter+len(text_slice)-1)]
+						right_slice = text[(start_letter+1):]
+						if DEBUG:
+							print "left is", left_slice, "right is", right_slice, "and text_slice", text_slice, "is in one?", text_slice in left_slice or text_slice in right_slice
+						if text_slice in left_slice or text_slice in right_slice:
+							text_slice_list = [text_slice]
+							print ">>>>  adding", text_slice_list
+							self.combinations.update(text_slice_list)
+
+							print ">>>>  combinations are: ", self.combinations
+			 		except IndexError:
+			 			if DEBUG:
+			 				print "INDEX ERROR"
+			 			end_of_text = True
+			 			break
+			 		if end_of_expansion:
+						if DEBUG:
+							print "BREAK out of expansion"
+						continue
+			except IndexError:
+				if DEBUG:
+					print "INDEX ERROR"
+				break
+			if end_of_text:
+				if DEBUG:
+					print "SOMEONE THREW AN END OF TEXT!"
+				break
 		return self.combinations.most_common()
 
 	def snippets(self):
@@ -97,7 +110,7 @@ class Frequency(object):
 
 # have overlapping patterns for this analysis.
 # make analysis for non-overlapping patterns.
-
+# also, make distances between repeats
 
 
 		
